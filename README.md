514d83d54a78f5c20d34b4f9a904497f78cd8829
cacosandon <jiossandon@uc.cl>
2021-08-26 10:54:17 -0400

    feat(360-visualization): first jupyter with slurm job

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # 360-visualization
 IA LAB PUC Research on VLN task, visualization fo 360° indoor environments on Matterport simulator
+
+# Use
+
+For local forwarding on remote server use
+
+`ssh -L localhost:8889:scylla:9995 <user>@<host>`
+
+If you are connection to IA LAB Cluster, use:
+
+`sbatch /home/jiossandon/repos/360-visualization/jupyter.sh`
+
+and cancel after not use
+
+`scancel <process ID SLURM>` (you can review the ID with `sq`)
---
+++ jupyter_on_slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=caco-jupyter
+#SBATCH --ntasks=1                  # Correr una sola tarea
+#SBATCH --output=slurm/jupyter.log    # Nombre del output (%j se reemplaza por el ID del trabajo)
+#SBATCH --error=slurm/jupyter.log     # Output de errores (opcional)
+#SBATCH --partition=all
+#SBATCH --workdir=/home/jiossandon   # Direccion donde correr el trabajo
+
+pwd; hostname; date
+
+source virtualenv/r2r/bin/activate
+
+jupyter notebook --no-browser --port=9995 --ip="0.0.0.0" && jupyter notebook --no-browser --port=9995
